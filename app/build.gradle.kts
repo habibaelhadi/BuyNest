f16b68b14bd25ceaba953b13cb4ec40e980ad713
YoussefFayad14 <youssefadelfayad@gmail.com>
2025-06-16 12:28:07 +0300

    Feat: Implement Profile Screen UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,6 @@ dependencies {
     // Accompanist Pager
     implementation (libs.accompanist.pager)
     implementation (libs.accompanist.pager.indicators.v0340)
+    // Material icons
+    implementation("androidx.compose.material:material-icons-extended")
 }
--- app/src/main/java/com/example/buynest/views/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/buynest/views/profile/ProfileScreen.kt
@@ -1,27 +1,235 @@
 package com.example.buynest.views.profile
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.buynest.ui.theme.MainColor
 
 @Composable
 fun ProfileScreen() {
+    val scrollState = rememberScrollState()
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
+            .background(MainColor)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+                .padding(bottom = 220.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(330.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Center)
+                        .padding(top = 24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(48.dp))
+
+                    ProfileImagePicker()
+
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Text(
+                        "Youssef Fayad",
+                        color = Color.White,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        "yousseffayad@gmail.com",
+                        color = Color.White.copy(alpha = 0.8f),
+                        fontSize = 20.sp
+                    )
+                }
+
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(16.dp)
+                )
+            }
+        }
+
+        Surface(
+            shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp),
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 24.dp)
+                    .heightIn(min = 330.dp)
+            ) {
+                Text(
+                    text = "Account Overview",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                ProfileOption(icon = Icons.Default.Person, title = "Change User Name", iconColor = Color(0xFF9DD1F1))
+                Spacer(modifier = Modifier.height(12.dp))
+
+                ProfileOption(icon = Icons.Default.Email, title = "Change Email", iconColor = Color(0xFFA9EFA3))
+                Spacer(modifier = Modifier.height(12.dp))
+
+                ProfileOption(icon = Icons.Default.Lock, title = "Change Password", iconColor = Color(0xFFFFD9A0))
+                Spacer(modifier = Modifier.height(12.dp))
+
+                ProfileOption(icon = Icons.Default.Phone, title = "Change Phone", iconColor = Color(0xFFB5A9EF))
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ProfileOption(icon: ImageVector, title: String, iconColor: Color) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(36.dp)
+                .background(iconColor.copy(alpha = 0.2f), shape = CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(imageVector = icon, contentDescription = null, tint = iconColor, modifier = Modifier.size(20.dp))
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Text(
+            text = title,
+            fontSize = 15.sp,
+            color = Color.Black,
+            modifier = Modifier.weight(1f)
+        )
+
+        Icon(imageVector = Icons.Default.ChevronRight, contentDescription = null, tint = Color.Gray)
+    }
+}
+
+@Composable
+fun ProfileImagePicker() {
+    val context = LocalContext.current
+    var imageBitmap by remember { mutableStateOf<Bitmap?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let {
+            imageBitmap = loadBitmapFromUri(context, it)
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .size(150.dp)
+            .clickable { launcher.launch("image/*") },
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Profile Screen",
-            color = Color.Black,
-            style = MaterialTheme.typography.headlineMedium
-        )
+        Box(
+            modifier = Modifier
+                .size(140.dp)
+                .clip(CircleShape)
+                .border(4.dp, Color.Yellow, CircleShape)
+                .background(MainColor),
+            contentAlignment = Alignment.Center
+        ) {
+            if (imageBitmap != null) {
+                Image(
+                    bitmap = imageBitmap!!.asImageBitmap(),
+                    contentDescription = "Selected Profile Picture",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(CircleShape)
+                )
+            } else {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Default Profile Icon",
+                    tint = Color.White,
+                    modifier = Modifier.size(75.dp)
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .align(Alignment.BottomEnd)
+                .offset(x = 8.dp, y = 8.dp)
+                .background(Color.White, shape = CircleShape)
+                .border(2.dp, MainColor, CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.Edit,
+                contentDescription = "Camera Icon",
+                tint = MainColor,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+    }
+}
+
+
+fun loadBitmapFromUri(context: Context, uri: Uri): Bitmap? {
+    return try {
+        if (Build.VERSION.SDK_INT < 28) {
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+        } else {
+            val source = ImageDecoder.createSource(context.contentResolver, uri)
+            ImageDecoder.decodeBitmap(source)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
     }
 }
