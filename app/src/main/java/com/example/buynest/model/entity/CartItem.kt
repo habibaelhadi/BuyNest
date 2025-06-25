bf773b820467e900a8b5bb2810c385d54035ade5
nour <n01555251740@gmail.com>
2025-06-25 17:22:54 +0300

    fix conflict

--- app/src/main/java/com/example/buynest/model/entity/CartItem.kt
+++ app/src/main/java/com/example/buynest/model/entity/CartItem.kt
@@ -10,6 +10,6 @@ data class CartItem(
     val imageUrl: String,
     val quantity: Int,
     val variantId: String = "",
-    val maxQuantity : Int = 0
+    val maxQuantity : Int = 0,
     val currencySymbol: String = ""
 )
