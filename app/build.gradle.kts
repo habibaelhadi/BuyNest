31c2bb00f118ea84bcebba8ea6fe64fd54a64683
Habiba Elhadi <habibaelhadi9@gmail.com>
2025-06-26 22:30:09 +0300

    Add dependencies for unit and instrumented tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -152,6 +152,17 @@ dependencies {
     //koin test dependencies
     testImplementation("io.insert-koin:koin-test-junit4")
     testImplementation("io.insert-koin:koin-android-test")
+    //test dependencies
+    androidTestImplementation ("io.mockk:mockk-android:1.13.17")
+    androidTestImplementation ("io.mockk:mockk-agent:1.13.17")
+    testImplementation("io.mockk:mockk-android:1.13.17")
+    testImplementation("io.mockk:mockk-agent:1.13.17")
+    testImplementation ("androidx.test:core-ktx:1.6.1")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.0")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test:core-ktx:1.6.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation ("org.robolectric:robolectric:4.11")
 }
 
 apollo {
